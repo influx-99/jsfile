0c0a585f8e8c19fb81df9524c2317adab656d491
influx-99 <32542629+influx-99@users.noreply.github.com>
2017-10-05 17:45:58 +0530

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,95 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "infile.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 114608,
+        "faces"         : 112233,
+        "normals"       : 134816,
+        "uvs"           : 129590,
+        "materials"     : 13
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "areaa"
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "areab"
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "areae"
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "areac"
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "aread"
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "inner"
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "error"
+	},
+
+	{
+	"DbgColor" : 3668136,
+	"DbgIndex" : 7,
+	"DbgName" : "collar"
+	},
+
+	{
+	"DbgColor" : 14968288,
+	"DbgIndex" : 8,
+	"DbgName" : "error"
+	},
+
+	{
+	"DbgColor" : 499914,
+	"DbgIndex" : 9,
+	"DbgName" : "stitching"
+	},
+
+	{
+	"DbgColor" : 1458591,
+	"DbgIndex" : 10,
+	"DbgName" : "sole"
+	},
+
+	{
+	"DbgColor" : 7078673,
+	"DbgIndex" : 11,
+	"DbgName" : "cotton_lace"
+	},
+
+	{
+	"DbgColor" : 8478454,
+	"DbgIndex" : 12,
+	"DbgName" : "tipcover"
+	}],
+
+    "buffers": "binfile.bin"
+
+}
